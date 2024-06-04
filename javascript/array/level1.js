50c8142f2b533d612291070f8d53ab8e169ce18e
Alka Rai <mrsalka02@gmail.com>
2024-06-04 17:33:31 +0545

    commit

---
+++ javascript/array/level1.js
@@ -0,0 +1,17 @@
+//Q1 find list of numbers divisible by 3
+
+const arr = [3,5,1,2,9,12]
+//write your code here
+const result = arr.filter(function alka(num) {
+    return num %3 == 0;
+});
+
+console.log(result);
+
+//expected output---> [3,9,12]
+// hint: %3 === 0
+// filter
+//syntax:
+// arr.filter((item,id)=>{
+//     //
+// })
